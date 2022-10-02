9cc6986effc538c2e6d91e0df85d77d0cd141e08
MiBBy-4 <maksim.762373maks@bk.ru>
2022-10-02 15:32:19 +0300

    feat: implement News model with assotiation with User

---
+++ app/models/news.rb
@@ -0,0 +1,6 @@
+class News < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 2, maximum: 250 }
+  validates :description, presence: true, length: {minimum: 2, maximum: 2000}
+
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_secure_password
   has_many :projects, dependent: :destroy
+  has_many :news, dependent: :destroy
 end
