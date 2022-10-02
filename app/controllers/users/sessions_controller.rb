f3a069b4403ad8e81d61c410b981f44fff24bc68
MiBBy-4 <maksim.762373maks@bk.ru>
2022-10-02 16:10:42 +0300

    refactor: rubocop helps in difficult times

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,6 @@
 
 module Users
   class SessionsController < ApplicationController
-
     def create
       user = User.find_by(email: params['users']['email']).try(:authenticate, params['users']['password'])
 
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class News < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2, maximum: 250 }
-  validates :description, presence: true, length: {minimum: 2, maximum: 2000}
+  validates :description, presence: true, length: { minimum: 2, maximum: 2000 }
 
   belongs_to :user
 end
