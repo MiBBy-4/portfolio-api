623607597fae5a97de55f043c17695216a47ba3c
MiBBy-4 <maksim.762373maks@bk.ru>
2022-10-02 15:44:35 +0300

    feat: generate News controller with necessery methods

---
+++ app/controllers/api/v1/news_controller.rb
@@ -0,0 +1,63 @@
+module Api
+  module V1
+    class NewsController < ApplicationController
+      before_action :set_news, only: %i[show update destroy]
+
+      def index
+        news = News.all
+        render json: news.to_json
+      end
+
+      def show
+        render json: @news
+      end
+
+      def create
+        news = News.new news_params
+
+        if news.save
+          render json: {
+            status: 201,
+            news: news
+          }
+        else
+          render json: {
+            status: 402,
+            errors: news.errors.full_messages
+          }
+        end
+      end
+
+      def update
+        if @news.update news_params
+          render json: {
+            status: 201,
+            news: news
+          }
+        else
+          render json: {
+            status: 402,
+            errors: news.errors.full_messages
+          }
+        end
+      end
+
+      def destroy
+        @news.destroy
+        render json: {
+          status: 202
+        }
+      end
+
+      private
+
+      def news_params
+        params.require(:news).permit(:title, :description)
+      end
+
+      def set_news
+        @news = News.find(params[:news_id])
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :projects, except: %i[new edit]
+      resources :news, except: %i[new edit]
     end
   end
 
